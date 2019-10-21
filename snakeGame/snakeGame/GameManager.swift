fe7d49b2ce1489978f1e0c36dc5980646410c493
Tamara <tamara.erlij@gmail.com>
2019-10-20 21:41:32 -0300

    jogo funcionando

---
+++ snakeGame/snakeGame/GameManager.swift
@@ -0,0 +1,182 @@
+//
+//  GameManager.swift
+//  snakeGame
+//
+//  Created by Tamara Erlij on 20/10/19.
+//  Copyright © 2019 Tamara Erlij. All rights reserved.
+//
+import Foundation
+import SpriteKit
+
+enum PlayerDirection {
+    case LEFT
+    case RIGHT
+    case UP
+    case DOWN
+    case DIED
+}
+
+class GameManager {
+    
+    private var scene: GameScene!
+    var numRows: Int!
+    var numCols: Int!
+    
+    private var nextTime: Double?
+    private var timeExtension: Double = 0.12
+    private var playerDirection: PlayerDirection = .LEFT
+    private var playerPositions: [Point] = []
+    
+    private var scorePos: Point?
+    private var currentScore: Int = 0
+    
+    init(scene: GameScene, numRows: Int, numCols: Int) {
+        self.scene = scene
+        self.numRows = numRows
+        self.numCols = numCols
+    }
+    
+    func initGame() {
+        //starting player position
+        playerPositions.append(Point(10, 10))
+        playerPositions.append(Point(10, 11))
+        playerPositions.append(Point(10, 12))
+        playerDirection = .LEFT
+        renderChange()
+        generateNewScorePos()
+    }
+
+    func update(time: Double) {
+        if nextTime == nil {
+            nextTime = time + timeExtension
+        } else if time >= nextTime! {
+            nextTime = time + timeExtension
+            if playerPositions.count > 0 {
+                updatePlayerPosition()
+                checkForScore()
+                checkPlayerDied()
+            } else if playerPositions.count == 0 && playerDirection == .DIED { // If no more snake and died
+                playerPositions.removeAll()
+                playerDirection = .LEFT // Change direction
+                renderChange()
+                scene.finishAnimation()
+            }
+        }
+    }
+    
+    func changeDirection(_ direction: PlayerDirection) {
+        if playerDirection == .DIED { return }
+        
+        if playerDirection == .LEFT && direction != .RIGHT { playerDirection = direction }
+        else if playerDirection == .RIGHT && direction != .LEFT { playerDirection = direction }
+        else if playerDirection == .UP && direction != .DOWN { playerDirection = direction }
+        else if playerDirection == .DOWN && direction != .UP { playerDirection = direction }
+    }
+    
+    private func updatePlayerPosition() {
+        // Init changes like if user died
+        var xChange = 0
+        var yChange = 0
+        if playerDirection == .LEFT {
+            xChange = -1
+        } else if playerDirection == .RIGHT {
+            xChange = 1
+        } else if playerDirection == .UP {
+            yChange = -1
+        } else if playerDirection == .DOWN {
+            yChange = 1
+        }
+
+        if playerPositions.count > 0 {
+            if playerDirection == .DIED {
+                playerPositions.removeLast()
+            } else {
+                var start = playerPositions.count - 1
+                while start > 0 {
+                    playerPositions[start] = playerPositions[start - 1]
+                    start -= 1
+                }
+                playerPositions[0] = Point(
+                    playerPositions[0].x + xChange,
+                    playerPositions[0].y + yChange
+                )
+            }
+        }
+        // Avoid snake go outside screen
+        if playerPositions.count > 0 {
+            let x = playerPositions[0].x
+            let y = playerPositions[0].y
+            if y >= numRows {
+                playerPositions[0].y = 0
+            } else if y < 0 {
+                playerPositions[0].y = numRows - 1
+            }
+            if x >= numCols {
+                playerPositions[0].x = 0
+            } else if x < 0 {
+                playerPositions[0].x = numCols - 1
+            }
+        }
+        renderChange()
+    }
+    
+    func checkForScore() {
+        if scorePos != nil && playerPositions.count > 0 {
+            let playerPos: Point = playerPositions[0]
+            if playerPos.equals(scorePos!) { // Player hit scorePos
+                currentScore += 1
+                scene.currentScore.text = "Score: \(currentScore)"
+                generateNewScorePos()
+                playerPositions.append(playerPositions.last!)
+                playerPositions.append(playerPositions.last!)
+                playerPositions.append(playerPositions.last!)
+            }
+        }
+    }
+    
+    func generateNewScorePos() {
+        var p: Point? = nil
+        // While score point is at same of player position, generate new one
+        while p == nil || contains(allPoint: playerPositions, point: p!) {
+            p = Point(Int.random(in: 0 ..< numCols), Int.random(in: 0 ..< numRows))
+        }
+        scorePos = p!
+    }
+    
+    func checkPlayerDied() {
+        if playerPositions.count > 0 {
+            var positions = playerPositions.filter { _ in return true }
+            let headSnake = positions[0]
+            positions.remove(at: 0)
+            if contains(allPoint: positions, point: headSnake) {
+                changeDirection(.DIED)
+                return
+            }
+        }
+    }
+
+    func renderChange() {
+        for (node, point) in scene.gameArray {
+            let isScorePos = scorePos != nil && point.equals(scorePos!)
+            let isPlayerPos = contains(allPoint: playerPositions, point: point)
+            
+            if isPlayerPos && playerDirection != .DIED {
+                node.fillColor = SKColor.green
+            } else if isScorePos {
+                node.fillColor = SKColor.red
+            } else if isPlayerPos && playerDirection == .DIED {
+                node.fillColor = SKColor.orange
+            } else {
+                node.fillColor = SKColor.clear
+            }
+        }
+    }
+
+    func contains(allPoint: [Point], point: Point) -> Bool {
+        for p in allPoint {
+            if point.x == p.x && point.y == p.y { return true }
+        }
+        return false
+    }
+}
+
--- snakeGame/snakeGame/GameScene.swift
+++ snakeGame/snakeGame/GameScene.swift
@@ -9,81 +9,191 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class Point {
+    var x: Int
+    var y: Int
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    func equals(_ other: Point) -> Bool {
+        return self.x == other.x && self.y == other.y
     }
 }
+
+
+class GameScene: SKScene {
+    // Main screen
+       private var gameLogo: SKLabelNode!
+       private var bestScore: SKLabelNode!
+       private var playButton: SKShapeNode!
+       // Died screen
+       private var youDiedText: SKLabelNode!
+       
+       // Game screen
+       private var game: GameManager!
+       var currentScore: SKLabelNode!
+       private var gameBG: SKShapeNode!
+       var gameArray: [(node: SKShapeNode, point: Point)] = []
+       
+       private var scaleOutAction: SKAction!
+       private var scaleInAction: SKAction!
+       private var moveOutsideTopAction: SKAction!
+       private var moveBackFromTopAction: SKAction!
+       
+   
+    override func didMove(to view: SKView) {
+    scaleInAction = SKAction.init(named: "ScaleIn")
+              scaleOutAction = SKAction.init(named: "ScaleOut")
+              moveOutsideTopAction = SKAction.init(named: "MoveOutsideTop")
+              moveBackFromTopAction = SKAction.init(named: "MoveBackFromTop")
+              
+              initializeMenu()
+              game = GameManager(scene: self, numRows: 30, numCols: 20)
+              initializeGameView()
+              
+              let swipeRight: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeR))
+              swipeRight.direction = .right
+              view.addGestureRecognizer(swipeRight)
+              
+              let swipeLeft: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeL))
+              swipeLeft.direction = .left
+              view.addGestureRecognizer(swipeLeft)
+              
+              let swipeUp: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeU))
+              swipeUp.direction = .up
+              view.addGestureRecognizer(swipeUp)
+              
+              let swipeDown: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeD))
+              swipeDown.direction = .down
+              view.addGestureRecognizer(swipeDown)
+          }
+          
+          @objc func swipeR() { game.changeDirection(.RIGHT) }
+          @objc func swipeL() { game.changeDirection(.LEFT) }
+          @objc func swipeU() { game.changeDirection(.UP) }
+          @objc func swipeD() {  game.changeDirection(.DOWN) }
+          
+          override func update(_ currentTime: TimeInterval) {
+              game.update(time: currentTime)
+          }
+          
+          override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+              for touch in touches {
+                  let location = touch.location(in: self)
+                  let touchedNode = nodes(at: location)
+                  for node in touchedNode {
+                      if node.name == "play_button" { startGame() }
+                  }
+              }
+          }
+          
+          private func startGame() {
+              self.gameBG.setScale(0)
+              self.currentScore.setScale(0)
+              
+              playButton.run(scaleOutAction) {
+                  self.playButton.isHidden = true
+              }
+
+              gameLogo.run(moveOutsideTopAction) {
+                  self.gameLogo.isHidden = true
+                  self.gameBG.isHidden = false
+                  self.currentScore.isHidden = false
+                  self.currentScore.run(self.scaleInAction)
+                  self.gameBG.run(self.scaleInAction) {
+                      // Init game
+                      self.game.initGame()
+                  }
+              }
+          }
+          
+          private func initializeMenu() {
+              //Create game title
+              gameLogo = SKLabelNode(fontNamed: "Avenir Next")
+              gameLogo.zPosition = 1
+              gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 200)
+              gameLogo.fontSize = 60
+              gameLogo.text = "SNAKE"
+              gameLogo.fontColor = SKColor.red
+              self.addChild(gameLogo)
+              //Create play button
+              playButton = SKShapeNode()
+              playButton.name = "play_button"
+              playButton.zPosition = 1
+              playButton.position = CGPoint(x: 0, y: (frame.size.height / -2) + 200)
+              playButton.fillColor = SKColor.green
+              let topCorner = CGPoint(x: -50, y: 50)
+              let bottomCorner = CGPoint(x: -50, y: -50)
+              let middle = CGPoint(x: 50, y: 0)
+              let path = CGMutablePath()
+              path.addLine(to: topCorner)
+              path.addLines(between: [topCorner, bottomCorner, middle])
+              playButton.path = path
+              self.addChild(playButton)
+          }
+          
+          private func initializeGameView() {
+              currentScore = SKLabelNode(fontNamed: "Avenir Next")
+              currentScore.zPosition = 1
+              currentScore.position = CGPoint(x: 0, y: (frame.size.height / -2) + 60)
+              currentScore.fontSize = 40
+              currentScore.isHidden = true
+              currentScore.text = "Score: 0"
+              currentScore.fontColor = SKColor.white
+              self.addChild(currentScore)
+              
+              let width = frame.size.width - 200
+              let cellSize = width  / CGFloat(game.numCols)
+              let height = cellSize * CGFloat(game.numRows)
+
+              let rect = CGRect(x: -width / 2, y: -height / 2, width: width, height: height)
+              gameBG = SKShapeNode(rect: rect, cornerRadius: 0.02)
+              gameBG.fillColor = SKColor.darkGray
+              gameBG.zPosition = 2
+              gameBG.isHidden = true
+              self.addChild(gameBG)
+
+              createGameBoard(width: width, height: height, cellSize: cellSize)
+          }
+          
+          //create a game board, initialize array of cells
+          private func createGameBoard(width: CGFloat, height: CGFloat, cellSize: CGFloat) {
+              var x = -width / 2 + cellSize / 2
+              var y = height / 2 - cellSize / 2
+              //loop through rows and columns, create cells
+              for i in 0...game.numRows - 1 {
+                  for j in 0...game.numCols - 1 {
+                      let cellNode = SKShapeNode(rectOf: CGSize(width: cellSize, height: cellSize))
+                      cellNode.strokeColor = SKColor.black
+                      cellNode.zPosition = 2
+                      cellNode.position = CGPoint(x: x, y: y)
+                      //add to array of cells -- then add to game board
+                      gameArray.append((node: cellNode, point: Point(j, i)))
+                      gameBG.addChild(cellNode)
+                      //iterate x
+                      x += cellSize
+                  }
+                  //reset x, iterate y
+                  x = CGFloat(width / -2) + (cellSize / 2)
+                  y -= cellSize
+              }
+          }
+          
+          func finishAnimation() {
+              currentScore.run(scaleOutAction) {
+                  self.currentScore.isHidden = true
+              }
+              gameBG.run(scaleOutAction) {
+                  self.gameBG.isHidden = true
+                  self.gameLogo.isHidden = false
+                  self.gameLogo.run(self.moveBackFromTopAction) {
+                      self.playButton.isHidden = false
+                      self.playButton.run(self.scaleInAction)
+                  }
+              }
+          }
+
+      }
